951cabbe1af01f11aecc2de332ffd571abb7bf0a
kiosses <kiosses@softweb.gr>
2021-04-21 11:14:21 +0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback ) => {
+    const url = 'http://api.weatherstack.com/current?access_key=' + process.env.WEATHERSTACK_KEY + '&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json:true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback({
+                Code: body.error.code,
+                Type: body.error.type
+            }, undefined)
+        } else {
+            const { current: { temperature, feelslike, weather_descriptions } } = body
+            const weatherForecast = `${weather_descriptions[0]}. It is currently ${temperature} degrees out, it feels like ${feelslike}.`
+            callback(undefined, weatherForecast)
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + process.env.MAPBOX_KEY + '&limit=1'
+
+    request({ url, json:true }, (error, { body: { features } }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            const {center, place_name} = features[0]
+            callback(undefined, {
+                latitude: center[1],
+                longitude:center[0],
+                place_name: place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
